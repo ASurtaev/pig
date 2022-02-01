15540e3e81ef52ab3e41e4dc29ef4e3f75fa352c
ASurtaev <alsurtaev@gmail.com>
2022-02-01 15:09:35 +0700

    update setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pig',
-      version='0.0',
-      url='https://github.com/Perfectrum/pig',
+      version='0.1',
+      url='https://github.com/ASurtaev/pig',
       packages=find_packages(),
      )
